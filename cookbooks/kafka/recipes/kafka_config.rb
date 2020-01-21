085d41244703b7c0c16ebc84d48fe5ef546aecea
fadlinurhasan <fadlinurhasan@users.noreply.github.com>
2020-01-21 15:25:38 +0700

    Configure Kafka so it still works if a single box is died. (#12)
    * Add min.insync.replicas config condition
    
    Co-authored-by: bentol <gnu.iben@gmail.com>
    
    * berks update
    
    Co-authored-by: bentol <gnu.iben@gmail.com>
    
    * set min insync replicas as attribute
    
    Co-authored-by: bentol <gnu.iben@gmail.com>
    
    * berks update
    
    Co-authored-by: bentol <gnu.iben@gmail.com>
    
    * update config attribute
    
    Co-authored-by: bentol <gnu.iben@gmail.com>
    
    * berks update
    
    Co-authored-by: bentol <gnu.iben@gmail.com>
    
    Co-authored-by: bentol <bentol@users.noreply.github.com>

--- cookbooks/kafka/recipes/kafka_config.rb
+++ cookbooks/kafka/recipes/kafka_config.rb
@@ -47,8 +47,7 @@ config['zookeeper.connect'] = zk_connection
 kafka_hosts_count = node[cookbook_name]['kafka']['hosts_count']
 if kafka_hosts_count < 3
   config['offsets.topic.replication.factor'] = kafka_hosts_count
-else
-  config['offsets.topic.replication.factor'] = node[cookbook_name]['kafka']['max_replication_factor']
+  config['min.insync.replicas'] = 1
 end
 
 # Write configurations
